e3cba87a9bc6ae642d5bfb5a54198637632ce487
iniemamocny <156675700+iniemamocny@users.noreply.github.com>
2025-09-07 13:13:40 +0200

    style: format vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS ? '/MebloPlan/' : '/',
@@ -13,24 +13,16 @@ export default defineConfig({
     alias: {
       react: path.resolve(__dirname, 'node_modules/react'),
       'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
-      'react/jsx-runtime': path.resolve(
-        __dirname,
-        'node_modules/react/jsx-runtime.js',
-      ),
+      'react/jsx-runtime': path.resolve(__dirname, 'node_modules/react/jsx-runtime.js'),
       'react/jsx-dev-runtime': path.resolve(
         __dirname,
-        'node_modules/react/jsx-runtime.js',
-      ),
-    },
+        'node_modules/react/jsx-runtime.js'
+      )
+    }
   },
   optimizeDeps: {
-    include: [
-      'react',
-      'react-dom',
-      'react/jsx-runtime',
-      'react/jsx-dev-runtime',
-    ],
+    include: ['react', 'react-dom', 'react/jsx-runtime', 'react/jsx-dev-runtime']
   },
   server: { host: true },
-  build: { outDir: 'dist', assetsDir: 'assets' },
-});
+  build: { outDir: 'dist', assetsDir: 'assets' }
+})
